c88947c8805d816e5f4f904aba82bdd4044be038
goodtft <goodtft@163.com>
2026-09-17 10:08:38 +0800

    Create setup_device.c

---
+++ application/edge_agent/boards/QDTFT/QDTFT_2_8_ESP32_S3_Board/setup_device.c
@@ -0,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string.h>
+#include "esp_log.h"
+#include "esp_lcd_ili9341.h"
+#include "esp_lcd_touch_ft5x06.h"
+
+static const char *TAG = "QDTFT_SETUP_DEVICE";
+
+esp_err_t lcd_panel_factory_entry_t(esp_lcd_panel_io_handle_t io, const esp_lcd_panel_dev_config_t *panel_dev_config, esp_lcd_panel_handle_t *ret_panel)
+{
+    esp_lcd_panel_dev_config_t panel_dev_cfg = {0};
+    memcpy(&panel_dev_cfg, panel_dev_config, sizeof(esp_lcd_panel_dev_config_t));
+    esp_err_t ret = esp_lcd_new_panel_ili9341(io, &panel_dev_cfg, ret_panel);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "New ili9341 panel failed");
+    }
+    return ret;
+}
+
+esp_err_t lcd_touch_factory_entry_t(esp_lcd_panel_io_handle_t io, const esp_lcd_touch_config_t *touch_dev_config, esp_lcd_touch_handle_t *ret_touch)
+{
+    esp_err_t ret = esp_lcd_touch_new_i2c_ft5x06(io, touch_dev_config, ret_touch);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to create ft5x06 (FT6336G compatible) touch driver: %s", esp_err_to_name(ret));
+    }
+    return ret;
+}
